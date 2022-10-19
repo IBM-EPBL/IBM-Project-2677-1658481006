e10a215b48d1f1fdfba3cef9255c44228c5ed861
Surendhar <70703769+surendhar2026@users.noreply.github.com>
2022-10-19 22:05:39 +0530

    Add files via upload

---
+++ Assignments/Surendhar (Member 3)/Assignment 1/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+ 
+app = Flask(__name__)  
+
+@app.route('/', methods =["GET", "POST"])
+def fun():
+    if request.method == "POST":
+       
+       first_name = request.form.get("uname")
+       mail_id = request.form.get("umail")
+       mobile = request.form.get("uphn")
+       return "Your name is "+first_name+" "+"you have logged in with this mail id : "+mail_id+" and phone : "+mobile
+    return render_template("home.html")
+ 
+if __name__=='main':
+   app.run()
